15cfe13a314ba175979025abeddf1fe4a36f06d7
Andy Davies <1andydavies1@gmail.com>
2016-12-02 10:36:36 +0000

    tidy comments

--- public/login.php
+++ public/login.php
@@ -1,18 +1,16 @@
 <?php
     
-    // configuration
     require("../includes/config.php"); 
 
-    // if user reached page via GET (as by clicking a link or via redirect)
     if ($_SERVER["REQUEST_METHOD"] == "GET")
     {
-        // render form
         render("login_form.php", ["title" => "Log In"]);
     }
 
-    // else if user reached page via POST (as by submitting a form via POST)
+
     else if ($_SERVER["REQUEST_METHOD"] == "POST")
     {
+        
         // filter input
         $filtered_input = array();
         if (empty($_POST['username']) || empty($_POST['password']))
@@ -29,16 +27,13 @@
             $filtered_input['password'] = $_POST['password'];
         }
 
-
         // encrypt password
         $encrypted_password = password_hash($filtered_input['password'], PASSWORD_DEFAULT);
 
-        // sanitise output to db
+        // escape output to db
         $sanitised_sql = array();
         $sanitised_sql['username'] = mysqli_real_escape_string($filtered_input['username']);
-        $sanitised_sql['encrypted_password'] = mysqli_real_escape_string($encrypted_password);
-
-     
+        $sanitised_sql['encrypted_password'] = mysqli_real_escape_string($encrypted_password);   
 
         // query database for user
         $rows = query("SELECT * FROM users WHERE username = ?", $sanitised_sql['username']);
@@ -49,18 +44,14 @@
             // first (and only) row
             $row = $rows[0];
 
-            // compare hash of user's input against hash in database
             if ($encrypted_password === $row["hash"])
             {
-                // remember that user's now logged in by storing user's ID in session
                 $_SESSION["id"] = $row["id"];
 
-                // redirect to portfolio
                 redirect("/myLibrary/public/");
             }
         }
 
-        // else apologize
         apologize("Invalid username and/or password.");
     }
 ?>
