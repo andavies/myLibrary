43a8032e446daef195b80ee3d9bc267c1ef4bbe2
Andy Davies <1andydavies1@gmail.com>
2019-06-18 21:54:39 +0100

    Add database schema Should have been added in a previous commit but there was a .sql hiding in the gitignore.

---
+++ db/create-tables.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `hash` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+);
+
+CREATE TABLE IF NOT EXISTS `books` (
+    `isbn` varchar(13),
+    `title` varchar(255),
+    `author` varchar(255),
+    `ownername` varchar(255),
+    `ownerid` int(10),
+    `thumb` varchar(255),
+    `description` varchar(1023)
+);
